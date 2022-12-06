ea1c7a7808a313ae93c621ded3f878aaa2f7d3ce
huzhengen <huzhengen@163.com>
2022-12-06 16:06:21 +0800

    Update api documentation

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -3,10 +3,12 @@ require "rspec_api_documentation/dsl"
 
 resource "Validation_codes" do
   post "/api/v1/validation_codes" do
-    example "Request code" do
+    parameter :email, type: :string
+    let(:email) { "1@gmail.com" }
+    example "Request Code" do
       do_request
-
-      expect(status).to eq 400
+      expect(status).to eq 200
+      expect(response_body).to eq " "
     end
   end
 end
