6238cadd0f1767c984f9815c9546075a46f77bfb
huzhengen <huzhengen@163.com>
2022-12-07 19:45:43 +0800

    Update validation code

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,3 +1,14 @@
 class ValidationCode < ApplicationRecord
   validates :email, presence: true
+
+  before_create :generate_code
+  after_create :send_email
+
+  def generate_code
+    self.code = SecureRandom.random_number.to_s[2..7]
+  end
+
+  def send_email
+    UserMailer.welcome_email self.email
+  end
 end
