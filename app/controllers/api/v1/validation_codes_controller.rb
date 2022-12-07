78d2a0adc94bf9e73fa0b60484ccbbd3df8ffde9
huzhengen <huzhengen@163.com>
2022-12-07 20:09:53 +0800

    Sending frequent validation code

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
+    # render status: :too_many_requests
+    return render status: 429 if ValidationCode.exists?(email: params[:email], kind: "sign_in", created_at: 1.minute.ago..Time.now)
     validation_code = ValidationCode.new email: params[:email], kind: "sign_in"
     if validation_code.save
       render status: 200
