50638a7298ac9c17680a668bb7be11014d3f620a
huzhengen <huzhengen@163.com>
2022-12-07 20:16:17 +0800

    Testing of sending frequent code

--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe "ValidationCodes", type: :request do
       post "/api/v1/validation_codes", params: { email: "1@gmail.com" }
       expect(response).to have_http_status(200)
     end
+    it "send frequent code" do
+      post "/api/v1/validation_codes", params: { email: "1@gmail.com" }
+      expect(response).to have_http_status(200)
+      post "/api/v1/validation_codes", params: { email: "1@gmail.com" }
+      expect(response).to have_http_status(429)
+    end
   end
 end
