b66637e5801e1641f2c4e8cd646457699c434601
huzhengen <huzhengen@163.com>
2022-12-12 16:04:36 +0800

    Add sign in api documentation

---
+++ spec/acceptance/sessions_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+require "rspec_api_documentation/dsl"
+
+resource "Sessions" do
+  post "/api/v1/session" do
+    parameter :email, "Email", required: true
+    parameter :code, "Code", required: true
+    response_field :jwt, "Token for authenticating the user"
+    let(:email) { "1@gmail.com" }
+    let(:code) { "123456" }
+    example "Sign in" do
+      User.create email: email
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json["jwt"]).to be_a String
+    end
+  end
+end
