65c566a8e9ed8b5320be5b553945a32166aba4bc
huzhengen <huzhengen@163.com>
2022-12-12 21:39:51 +0800

    Check if the tags belong to the current user

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   validates :amount, presence: true
   validates :tags_id, presence: true
   validates :happen_at, presence: true
+
+  validate :check_tags_id_belong_to_user
+
+  def check_tags_id_belong_to_user
+    all_tag_ids = Tag.where({ user_id: self.user_id }).map(&:id)
+    if self.tags_id & all_tag_ids != self.tags_id
+      self.errors.add :tags_id, "Tag does not belong to the current user"
+    end
+  end
 end
