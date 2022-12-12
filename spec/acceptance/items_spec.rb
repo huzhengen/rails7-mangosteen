d46dd0a242c58200840e289abdaf9965f5d782f1
huzhengen <huzhengen@163.com>
2022-12-12 13:25:24 +0800

    Update api documentation

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -3,6 +3,7 @@ require "rspec_api_documentation/dsl"
 
 resource "Items" do
   get "/api/v1/items" do
+    authentication :basic, :auth
     parameter :page, "page"
     parameter :created_after, "start time"
     parameter :created_before, "end time"
@@ -12,8 +13,12 @@ resource "Items" do
     end
     let(:created_after) { "2022-10-10" }
     let(:created_before) { "2022-11-11" }
+    let(:current_user) { User.create email: "1@gmail.com" }
+    let(:auth) { "Bearer #{current_user.generate_jwt}" }
     example "Get items" do
-      11.times do Item.create amount: 100, created_at: "2022-10-30" end
+      11.times do
+        Item.create amount: 100, created_at: "2022-10-30", user_id: current_user.id
+      end
       do_request
       expect(status).to eq 200
       json = JSON.parse response_body
