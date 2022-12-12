d9a3a890af9020377bbe85b0a187bbf0de73bc15
huzhengen <huzhengen@163.com>
2022-12-12 15:01:56 +0800

    Token expired

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -4,9 +4,17 @@ class AutoJwt
   end
 
   def call(env)
+    return @app.call(env) if ["/api/v1/session", "/api/v1/validation_codes"].include? env["PATH_INFO"]
+
     header = env["HTTP_AUTHORIZATION"]
     jwt = header.split(" ")[1] rescue ""
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: "HS256" } rescue nil
+    begin
+      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: "HS256" }
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({ reason: "token expired" })]]
+    rescue
+      return [401, {}, [JSON.generate({ reason: "token invalid" })]]
+    end
     env["current_user_id"] = payload[0]["user_id"] rescue nil
     @status, @headers, @response = @app.call(env)
     [@status, @headers, @response]
--- spec/requests/api/v1/mes_spec.rb
+++ spec/requests/api/v1/mes_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
+require "active_support/testing/time_helpers"
 
 RSpec.describe "Me", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   describe "Get current user" do
     it "Get user after sign in" do
       user = User.create! email: "1@gmail.com"
       post "/api/v1/session", params: { email: "1@gmail.com", code: "123456" }
+      expect(response).to have_http_status(200)
       json = JSON.parse response.body
       jwt = json["jwt"]
       get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
@@ -12,6 +15,22 @@ RSpec.describe "Me", type: :request do
       json = JSON.parse response.body
       # expect(json["resource"]["id"]).to be_a(Integer)
       expect(json["resource"]["id"]).to eq user.id
+    end
+    it "jwt过期" do
+      travel_to Time.now - 3.hours
+      user1 = User.create! email: "1@gmail.com"
+      jwt = user1.generate_jwt
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(401)
+    end
+    it "jwt没过期" do
+      travel_to Time.now - 1.hours
+      user1 = User.create! email: "1@gmail.com"
+      jwt = user1.generate_jwt
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(200)
     end
   end
 end
