2859e54d88a33001bc1d450a0f1d64d8be43cdad
huzhengen <huzhengen@163.com>
2022-12-13 18:09:49 +0800

    Deploy

--- bin/pack_for_remote.sh
+++ bin/pack_for_remote.sh
@@ -8,7 +8,9 @@ current_dir=$(dirname $0)
 deploy_dir=/home/$user/deploys/$time
 gemfile=$current_dir/../Gemfile
 gemfile_lock=$current_dir/../Gemfile.lock
-vendor_cache_dir=$current_dir/../vendor/cache
+vendor_dir=$current_dir/../vendor
+vendor_1=rspec_api_documentation
+api_dir=$current_dir/../doc/api
 
 function title {
   echo 
@@ -19,22 +21,30 @@ function title {
 }
 
 
-title '打包源代码为压缩文件'
-mkdir $cache_dir
-bundle cache
-tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" -czv -f $dist *
+mkdir -p $cache_dir
+title '打包源代码'
+tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" --exclude="vendor/*" -cz -f $dist *
+title "打包本地依赖 ${vendor_1}"
+bundle cache --quiet
+tar -cz -f "$vendor_dir/cache.tar.gz" -C ./vendor cache
+tar -cz -f "$vendor_dir/$vendor_1.tar.gz" -C ./vendor $vendor_1
 title '创建远程目录'
 ssh $user@$ip "mkdir -p $deploy_dir/vendor"
-title '上传压缩文件'
+title '上传源代码和依赖'
 scp $dist $user@$ip:$deploy_dir/
 yes | rm $dist
 scp $gemfile $user@$ip:$deploy_dir/
 scp $gemfile_lock $user@$ip:$deploy_dir/
-scp -r $vendor_cache_dir $user@$ip:$deploy_dir/vendor/
+scp -r $vendor_dir/cache.tar.gz $user@$ip:$deploy_dir/vendor/
+yes | rm $vendor_dir/cache.tar.gz
+scp -r $vendor_dir/$vendor_1.tar.gz $user@$ip:$deploy_dir/vendor/
+yes | rm $vendor_dir/$vendor_1.tar.gz
 title '上传 Dockerfile'
 scp $current_dir/../config/host.Dockerfile $user@$ip:$deploy_dir/Dockerfile
 title '上传 setup 脚本'
 scp $current_dir/setup_remote.sh $user@$ip:$deploy_dir/
+title '上传 API 文档'
+scp -r $api_dir $user@$ip:$deploy_dir/
 title '上传版本号'
 ssh $user@$ip "echo $time > $deploy_dir/version"
 title '执行远程脚本'
--- bin/setup_remote.sh
+++ bin/setup_remote.sh
@@ -1,6 +1,7 @@
 user=mangosteen
 root=/home/$user/deploys/$version
 container_name=mangosteen-prod-1
+nginx_container_name=mangosteen-nginx-1
 db_container_name=db-for-mangosteen
 
 function set_env {
@@ -8,7 +9,7 @@ function set_env {
   hint=$2
   [[ ! -z "${!name}" ]] && return
   while [ -z "${!name}" ]; do
-    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:"
+    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:" 
     read $name
   done
   sed -i "1s/^/export $name=${!name}\n/" ~/.bashrc
@@ -25,7 +26,7 @@ function title {
 title '设置远程机器的环境变量'
 set_env DB_HOST
 set_env DB_PASSWORD
-set_env RAILS_MASTER_KEY
+set_env RAILS_MASTER_KEY '请将 config/credentials/production.key 的内容复制到这里'
 
 title '创建数据库'
 if [ "$(docker ps -aq -f name=^${DB_HOST}$)" ]; then
@@ -42,15 +43,15 @@ else
   echo '创建成功'
 fi
 
-title 'docker build'
+title 'app: docker build'
 docker build $root -t mangosteen:$version
 
 if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
-  title 'docker rm'
+  title 'app: docker rm'
   docker rm -f $container_name
 fi
 
-title 'docker run'
+title 'app: docker run'
 docker run -d -p 3000:3000 \
            --network=network1 \
            --name=$container_name \
@@ -67,5 +68,17 @@ case $ans in
     n|N|2  )  echo "no" ;;
     ""     )  echo "no" ;;
 esac
+
+if [ "$(docker ps -aq -f name=^${nginx_container_name}$)" ]; then
+  title 'doc: docker rm'
+  docker rm -f $nginx_container_name
+fi
+
+title 'doc: docker run'
+docker run -d -p 8080:80 \
+           --network=network1 \
+           --name=$nginx_container_name \
+           -v /home/$user/deploys/$version/api:/usr/share/nginx/html:ro \
+           nginx:latest
 
 title '全部执行完毕'
