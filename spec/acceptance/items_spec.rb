be3a24f49482e0c18a0e7c4d5c5321bbf7e7940c
huzhengen <huzhengen@163.com>
2022-12-13 17:39:27 +0800

    Update api docs

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -56,4 +56,58 @@ resource "Items" do
       expect(json["resource"]["user_id"]).to eq current_user.id
     end
   end
+
+  get "/api/v1/items/summary" do
+    parameter :happened_after, "Start time", required: true
+    parameter :happened_before, "End time", required: true
+    parameter :kind, "Kind", enum: ["expenses", "income"], required: true
+    parameter :group_by, "happen_at", enum: ["happen_at", "tag_id"], required: true
+    response_field :groups, "Group"
+    response_field :total, "Total"
+    let(:happened_after) { "2018-01-01" }
+    let(:happened_before) { "2019-01-01" }
+    let(:kind) { "expenses" }
+    example "Get items summary by happen_at" do
+      user = current_user
+      tag = Tag.create! name: "tag1", sign: "x", user_id: user.id
+      Item.create! amount: 100, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-18T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 200, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-18T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 100, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-20T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 200, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-20T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 100, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-19T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 200, kind: "expenses", tags_id: [tag.id], happen_at: "2018-06-19T00:00:00+08:00", user_id: user.id
+      do_request group_by: "happen_at"
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json["groups"].size).to eq 3
+      expect(json["groups"][0]["happen_at"]).to eq "2018-06-18"
+      expect(json["groups"][0]["amount"]).to eq 300
+      expect(json["groups"][1]["happen_at"]).to eq "2018-06-19"
+      expect(json["groups"][1]["amount"]).to eq 300
+      expect(json["groups"][2]["happen_at"]).to eq "2018-06-20"
+      expect(json["groups"][2]["amount"]).to eq 300
+      expect(json["total"]).to eq 900
+    end
+
+    example "Get items summary by tag_id" do
+      user = current_user
+      tag1 = Tag.create! name: "tag1", sign: "x", user_id: user.id
+      tag2 = Tag.create! name: "tag2", sign: "x", user_id: user.id
+      tag3 = Tag.create! name: "tag3", sign: "x", user_id: user.id
+      Item.create! amount: 100, kind: "expenses", tags_id: [tag1.id, tag2.id], happen_at: "2018-06-18T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 200, kind: "expenses", tags_id: [tag2.id, tag3.id], happen_at: "2018-06-18T00:00:00+08:00", user_id: user.id
+      Item.create! amount: 300, kind: "expenses", tags_id: [tag3.id, tag1.id], happen_at: "2018-06-18T00:00:00+08:00", user_id: user.id
+      do_request group_by: "tag_id"
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      expect(json["groups"].size).to eq 3
+      expect(json["groups"][0]["tag_id"]).to eq tag3.id
+      expect(json["groups"][0]["amount"]).to eq 500
+      expect(json["groups"][1]["tag_id"]).to eq tag1.id
+      expect(json["groups"][1]["amount"]).to eq 400
+      expect(json["groups"][2]["tag_id"]).to eq tag2.id
+      expect(json["groups"][2]["amount"]).to eq 300
+      expect(json["total"]).to eq 600
+    end
+  end
 end
