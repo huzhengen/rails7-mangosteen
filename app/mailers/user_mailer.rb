15f6da3baed1f6e19f64ecb40fd479328d11f797
huzhengen <huzhengen@163.com>
2023-04-26 17:13:54 +0800

    Have the code displayed in the subject of the email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(email)
     validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
-    mail(to: email, subject: "Welcome to My Awesome Site")
+    mail(to: email, subject: "【#{@code}】时空存钱罐验证码")
   end
 end
