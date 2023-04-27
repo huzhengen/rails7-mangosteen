ca05c31a484a0fa6fdd456ecc8b666dc8955d924
huzhengen <huzhengen@163.com>
2023-04-27 19:01:07 +0800

    Add me_spec.rb in acceptance

---
+++ spec/acceptance/me_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+require "rspec_api_documentation/dsl"
+
+resource "Me-current user" do
+  let(:current_user) { create :user }
+  let(:auth) { "Bearer #{current_user.generate_jwt}" }
+  get "/api/v1/me" do
+    authentication :basic, :auth
+    example "Get current user" do
+      do_request
+      expect(status).to eq 200
+      json = JSON.parse response_body
+      p json
+      expect(json["resource"]["id"]).to eq current_user.id
+    end
+  end
+end
