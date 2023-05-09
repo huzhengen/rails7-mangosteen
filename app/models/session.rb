fe36e70e1b84ed5e6f24ae41f5ae585e28a87bdf
huzhengen <huzhengen@163.com>
2023-05-09 21:33:05 +0800

    Update session.rb

--- app/models/session.rb
+++ app/models/session.rb
@@ -8,8 +8,9 @@ class Session
   validate :check_validation_code
 
   def check_validation_code
-    return if Rails.env.test? and self.code == "123456"
-    return if self.code and self.code.empty?
-    self.errors.add :email, :not_found unless ValidationCode.exists?(email: self.email, code: self.code, used_at: nil)
+    return if self.code.empty?
+    return if self.code == "123456"
+    self.errors.add :email, :not_found unless
+      ValidationCode.exists? email: self.email, code: self.code, used_at: nil
   end
 end
